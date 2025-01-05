056d35a90de0d2124c1b9f869683c8ea874924a6
Jephthah Orobia <115846010+jephthah-orobia@users.noreply.github.com>
2025-01-05 10:03:47 +0000

    add app description

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 /* ****************************************************************
- * @author: <your name here>
- * @app name: <your application name here>
- * @app desc: <describe the purpose of your application here>
- * @history:
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
+ * @author: Jephthah M. Orobia
+ * @app name: AMAOEd Machine Problem 3
+ * @app desc: To display payslips of employees
  * ****************************************************************/
 
 #include <cstdio>
@@ -20,25 +11,12 @@
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-//                               NOTE
-// This is your program entry point. Your main logic is placed
-// inside this function. You may add your functions before this
-// "main()", or after this "main()" provided you added reference
-// before this "main()" function.
-//////////////////////////////////////////////////////////////////
 
 int main()
 {
-  // ************************** TO DO **************************
-  // Place your code logic after this comment line
-  // ***********************************************************
 
   cout << "Hello World!" << endl;
 
-  // ********************** DO NOT CHANGE **********************
-  // Print a new line and ask user for any key before exiting
-  // ***********************************************************
   _pause();
   return EXIT_SUCCESS;
 }
